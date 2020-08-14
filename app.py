bdc45907f9a35d064bcb95219d3f367e5a9bddde
syth0le <chdan565@gamil.com>
2020-08-14 17:00:45 +0300

    image and descr support

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import hash_gen
 
-
 application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///code.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,11 +10,13 @@ db = SQLAlchemy(application)
 
 
 class Code(db.Model):
-    #id = db.Column(db.Integer)
+    # id = db.Column(db.Integer)
     name = db.Column(db.String(100), nullable=False)
     hash = db.Column(db.String(300), nullable=False, primary_key=True)
     code = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+    code_image = db.Column(db.BLOB, nullable=True)
+    description = db.Column(db.Text, nullable=True)
 
     ## TODO: Add fields
 
@@ -29,8 +30,8 @@ def show_hello_page():
     return render_template('home.html')
 
 
-#@application.route('/code/<int:id>')
-#def show_code_page(id):
+# @application.route('/code/<int:id>')
+# def show_code_page(id):
 #    code = Code.query.get(id)
 #    return render_template('code.html', code=code)
 
@@ -45,13 +46,13 @@ def code_update(hash):
     code = Code.query.get(hash)
     if request.method == 'POST':
         try:
-            code.name = request.form['name']
             code.code = request.form['code']
-            #code.date = request.form['date']
+            ## такие же поля для добавления текста сбоку и картинок
+            code.description = request.form['description']
         except:
             return redirect('/add_code')
             # render_template("add_code.html")
-            #redirect('/code/{}/add_code')
+            # redirect('/code/{}/add_code')
 
         try:
             db.session.commit()
@@ -64,8 +65,8 @@ def code_update(hash):
 
 
 """"@application.route('/code/<string:hash>/delete', methods=['POST', 'GET'])
-def code_delete(id):
-    code = Code.query.get_or_404(id)
+def code_delete(hash):
+    code = Code.query.get_or_404(hash)
 
     try:
         db.session.delete(code)
@@ -83,10 +84,10 @@ def add_code():
 
         count = len(Code.query.all()) - 1
         print(count)
-        #hash = request.form["{}".format(hash_gen.hash_password(count))]
+        # hash = request.form["{}".format(hash_gen.hash_password(count))]
 
         codeAdd = Code(name=name, code=code, hash=hash_gen.hash_password(count))
-        #print(codeAdd.hash)
+        # print(codeAdd.hash)
         try:
             db.session.add(codeAdd)
             print(1)
