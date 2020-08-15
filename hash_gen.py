6ff6902b03f83c8440962c4822b23d558fcd2174
Denis Putnov <grnbows.connect@mail.ru>
2020-08-15 12:55:43 +0400

    prerelease v1.0 refactoring

--- hash_gen.py
+++ hash_gen.py
@@ -5,7 +5,8 @@ import hashlib
 def hash_password(password):
     # uuid используется для генерации случайного числа
     salt = uuid.uuid4().hex
-    return hashlib.sha256(str(salt).encode() + str(password).encode()).hexdigest() + ':' + salt
+    hash = hashlib.sha256(str(salt).encode() + str(password).encode()).hexdigest() + ':' + salt
+    return hash[5:20]
 
 
 def check_password(hashed_password, user_password):
